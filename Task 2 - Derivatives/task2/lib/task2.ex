608075d3fe370c0ce1223d15189a817ba8d826ae
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-01-25 11:32:46 +0100

    Calculate method with specific x value

--- Task 2 - Derivatives/task2/lib/task2.ex
+++ Task 2 - Derivatives/task2/lib/task2.ex
@@ -16,7 +16,7 @@ defmodule Derivative do
     IO.write("Expression: #{pprint(e)}\n")
     IO.write("Derivative: #{pprint(d1)}\n")
     IO.write("Simplified: #{pprint(simplify(d1))}\n")
-    IO.write("Calculated: #{c}\n")
+    IO.write("Calculated: #{pprint(simplify(c))}\n")
   end
 
   def test2() do
@@ -36,18 +36,15 @@ defmodule Derivative do
   def deriv({:num, _}, _) do {:num, 0} end
   def deriv({:var, v}, v) do {:num, 1} end
   def deriv({:var, _}, _) do {:num, 0} end
-
   def deriv({:add, e1, e2}, v) do
     {:add, deriv(e1, v), deriv(e2, v)}
   end
-
   def deriv({:mul, e1, e2}, v) do
     {:add,
       {:mul, deriv(e1, v), e2},
       {:mul, e1, deriv(e2, v)}
     }
   end
-
   def deriv({:exp, e, {:num, n}}, v) do
     {:mul,
       {:mul, {:num, n}, {:exp, e, {:num, n-1}}},
@@ -56,7 +53,18 @@ defmodule Derivative do
   end
 
 
-
+  def calc({:num, n}, _, _) do {:num, n} end
+  def calc({:var, v}, v, n) do {:num, n} end
+  def calc({:var, v}, _, _) do {:num, v} end
+  def calc({:add, e1, e2}, v, n) do
+    {:add, calc(e1, v, n), calc(e2, v, n)}
+  end
+  def calc({:mul, e1, e2}, v, n) do
+    {:mul, calc(e1, v, n), calc(e2, v, n)}
+  end
+  def calc({:exp, e1, e2}, v, n) do
+    {:exp, calc(e1, v, n), calc(e2, v, n)}
+  end
 
 
   def simplify({:add, e1, e2}) do
@@ -84,6 +92,7 @@ defmodule Derivative do
 
   def simplify_exp(e1, {:num, 1}) do e1 end
   def simplify_exp(_, {:num, 0}) do {:num, 1} end
+  def simplify_exp({:num, n}, {:num, m}) do {:num, n ** m} end
   def simplify_exp(e1, e2) do {:exp, e1, e2} end
 
 
