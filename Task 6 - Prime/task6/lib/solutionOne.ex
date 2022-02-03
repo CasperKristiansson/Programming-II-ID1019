6494b1f1272108bfdc5bae60688967995bad0df8
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-03 15:26:12 +0100

    Created task1, part one done

---
+++ Task 6 - Prime/task6/lib/solutionOne.ex
@@ -0,0 +1,24 @@
+defmodule SolutioneOne do
+  @moduledoc """
+  Create a list of all integers between 2 and the given number n.
+  You can use the call Enum.to_list (2..n) to create the list of numbers.
+  The first number in the list is a prime number and it should be the first
+  number in the list that we generate. Now remove all numbers in the rest of
+  the list that are divisible by the first number, use the function rem / 2.
+  If the result is an empty list, we are ready, otherwise you have found the
+  next prime number.
+  """
+
+  def primes(n) do
+    lst = Enum.to_list(2..n)
+    recursionFilter(lst)
+  end
+
+  def recursionFilter([head | tail]) do
+    if tail == [] do
+      [head]
+    else
+      [head | recursionFilter(Enum.filter(tail, fn(x) -> rem(x, head) != 0 end))]
+    end
+  end
+end
---
+++ Task 6 - Prime/task6/lib/test.ex
@@ -0,0 +1,8 @@
+defmodule Test do
+  @moduledoc """
+  This is a test module for the three different subtasks.
+  """
+  def test1 do
+    SolutioneOne.primes(100)
+  end
+end
