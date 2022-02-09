f8934caa01899bb085ea0d3b7f9bf978d1350d1f
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-09 14:14:08 +0100

    Task 8 Part two done

--- Task 8 - Advent of Code/task8/lib/solution.ex
+++ Task 8 - Advent of Code/task8/lib/solution.ex
@@ -17,4 +17,29 @@ defmodule Solution do
       measurementsIncreasing(tail, head, result)
     end
   end
+
+  def partTwo() do
+    data = Data.readData()
+    sum = getThreeValue(data, 0, 0)
+    measureSlidingWindow(data, sum, 0)
+  end
+
+  def getThreeValue([], _, sum) do sum end
+  def getThreeValue([head | tail], value, sum) do
+    if value < 3 do
+      getThreeValue(tail, value + 1, sum + head)
+    else
+      sum
+    end
+  end
+
+  def measureSlidingWindow([], _, result) do result end
+  def measureSlidingWindow([head | tail], value, result) do
+    newValue = getThreeValue([head | tail], 0, 0)
+    if newValue > value do
+      measureSlidingWindow(tail, newValue, result + 1)
+    else
+      measureSlidingWindow(tail, newValue, result)
+    end
+  end
 end
