3149822bb925fc28b68757c3fab327799665b30d
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-17 08:19:39 +0100

    Created mix project task10, task9. Task 10 list processing done

---
+++ Task 10 - Train shunting/task10/lib/processing.ex
@@ -0,0 +1,40 @@
+defmodule Processing do
+  @moduledoc """
+  Containts list processing routines for the train shunting.
+  """
+
+  @doc """
+  Returns the list containing the first n elements of xs.
+  """
+  def take([], _) do [] end
+  def take(_, 0) do [] end
+  def take([head | tail], n) do
+    [head] ++ take(tail, n - 1)
+  end
+
+  @doc """
+  Returns the list xs without its first n elements.
+  """
+  def drop([], _) do [] end
+  def drop(xs, 0) do xs end
+  def drop([_ | tail], n) do
+    drop(tail, n - 1)
+  end
+
+  @doc """
+  Returns the list xs with the elements of ys appended.
+  """
+  def append(xs, ys) do xs ++ ys end
+
+  @doc """
+  Tests whether y is an element of xs.
+  """
+  def member(xs, y) do Enum.member?(xs, y) end
+
+  @doc """
+  Returns the first position of y in the list xs.
+  """
+  def position(xs, y) do
+    Enum.find_index(xs, fn(x) -> x == y end)
+  end
+end
---
+++ Task 10 - Train shunting/task10/test/task10_test.exs
@@ -0,0 +1,8 @@
+defmodule Task10Test do
+  use ExUnit.Case
+  doctest Task10
+
+  test "greets the world" do
+    assert Task10.hello() == :world
+  end
+end
