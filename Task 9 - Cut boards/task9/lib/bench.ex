3323b0ef428cd5de47c2d95289b71b03c0e15143
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-21 09:11:57 +0100

    Task 9 Calculating cost using memo

--- Task 9 - Cut boards/task9/lib/bench.ex
+++ Task 9 - Cut boards/task9/lib/bench.ex
@@ -1,7 +1,14 @@
 defmodule Bench do
-  def bench(n) do
+  def benchOne(n) do
     for i <- 1..n do
       {t, _} = :timer.tc(fn() -> Lumber.cost(Enum.to_list(1..i)) end)
+      IO.puts(" n = #{i}\t t = #{t} us")
+    end
+  end
+
+  def benchTwo(n) do
+    for i <- 1..n do
+      {t, _} = :timer.tc(fn() -> Lumber.costMemo(Enum.to_list(1..i)) end)
       IO.puts(" n = #{i}\t t = #{t} us")
     end
   end
--- Task 9 - Cut boards/task9/lib/lumber.ex
+++ Task 9 - Cut boards/task9/lib/lumber.ex
@@ -29,4 +29,46 @@ defmodule Lumber do
       costRight
     end
   end
+
+
+  def costMemo([]) do {0, :na} end
+  def costMemo(seq) do
+    {cost, tree, _} = costMemo(seq, Memo.new())
+    {cost, tree}
+  end
+  def costMemo([s], mem) do {0, s, mem} end
+  def costMemo(seq, mem) do
+    {c, t, mem} = costMemo(seq, 0, [], [], mem)
+    {c, t, Memo.add(mem, seq, {c, t})}
+  end
+  def check(seq, mem) do
+    case Memo.lookup(mem, seq) do
+      nil ->
+        costMemo(seq, mem)
+      {c, t} ->
+        {c, t, mem}
+    end
+  end
+  def costMemo([], l, left, right, mem) do
+    {costLeft, treeLeft, mem} = check(left, mem)
+    {costRight, treeRight, mem} = check(right, mem)
+    {costLeft + costRight + l, {treeLeft, treeRight}, mem}
+  end
+  def costMemo([s], l, left, [], mem) do
+    {cost, tree, mem} = check(left, mem)
+    {cost + s + l, {s, tree}, mem}
+  end
+  def costMemo([s], l, [], right, mem) do
+    {cost, tree, mem} = check(right, mem)
+    {cost + s + l, {tree, s}, mem}
+  end
+  def costMemo([s | rest], l, left, right, mem) do
+    {costLeft, treeLeft, mem} = costMemo(rest, s + l, [s | left], right, mem)
+    {costRight, treeRight, mem} = costMemo(rest, s + l, left, [s | right], mem)
+    if costLeft < costRight do
+      {costLeft + costRight + l, {treeLeft, treeRight}, mem}
+    else
+      {costRight + costLeft + l, {treeRight, treeLeft}, mem}
+    end
+  end
 end
