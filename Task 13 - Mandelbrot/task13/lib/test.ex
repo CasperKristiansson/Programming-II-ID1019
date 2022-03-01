629ae92dd6d7179e4cce1a175d673958821f825b
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-03-01 10:19:31 +0100

    Updated report for task11

--- Task 13 - Mandelbrot/task13/lib/test.ex
+++ Task 13 - Mandelbrot/task13/lib/test.ex
@@ -1,18 +1,19 @@
 defmodule Test do
   def demo() do
-    small(-0.001, 0.663, 0.008)
+    small(-0.00001, 0.642, 0.008)
   end
 
   # -2.4, 1.3, 2.2
   # -0.001, 0.765, 0.013
   # -0.001, 0.663, 0.008
+  # -0.00001, 0.642, 0.008
   def save() do
-    big(-0.001, 0.663, 0.008)
+    big(-0.00001, 0.642, 0.008)
   end
   def small(x0, y0, xn) do
-    width = 1920
-    height = 1080
-    depth = 256 * 1.3
+    width = 720
+    height = 480
+    depth = 256 * 3
     k = (xn - x0) / width
     image = Mandel.mandelbrot(width, height, x0, y0, k, depth)
     PPM.write("running.ppm", image)
@@ -21,9 +22,9 @@ defmodule Test do
   def big(x0, y0, xn) do
     width = 2560
     height = 1440
-    depth = 256 * 1.3
+    depth = 256 * 3
     k = (xn - x0) / width
     image = Mandel.mandelbrot(width, height, x0, y0, k, depth)
-    PPM.write("Zoomed Branches.ppm", image)
+    PPM.write("Zoomed Spiral.ppm", image)
   end
 end
